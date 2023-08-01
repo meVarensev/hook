b3db0467fb5b1223ae234fac7c079e1c360d8f83
meVarensev <vitalik.unit@gmail.com>
2023-08-01 19:37:45 +0500

    fix: class name test

--- lib/class-names/__tests__/class-names.test.ts
+++ lib/class-names/__tests__/class-names.test.ts
@@ -1,12 +1,20 @@
-import { classNames } from '../class-names';
+import {classNames, Mods} from '../class-names';
 
-test.concurrent.each([
+const testCases: [string, Mods, string[], string][] = [
     ['base-class', {}, [], 'base-class'],
-    ['base-class', { active: true, large: true, disabled: false }, ['extra-class', 'more-classes'], 'base-class extra-class more-classes active large'],
-    ['base-class', { active: true, large: false, disabled: true }, [], 'base-class active disabled'],
-    ['base-class', { active: true, large: undefined, disabled: true }, [], 'base-class active disabled'],
-])('classNames function should behave correctly with different inputs', async (cls, mods, additional, expected) => {
-    const result = classNames(cls, mods, additional);
+    ['base-class', {
+        active: true,
+        large: true,
+        disabled: false
+    }, ['extra-class', 'more-classes'], 'base-class extra-class more-classes active large'],
+    ['base-class', {active: true, large: false, disabled: true}, [], 'base-class active disabled'],
+    ['base-class', {active: true, large: undefined, disabled: true}, [], 'base-class active disabled'],
+];
 
-    expect(result).toBe(expected);
-});
+test.concurrent.each(testCases)(
+    'classNames function should behave correctly with different inputs',
+    async (cls, mods, additional, expected) => {
+        const result = classNames(cls, mods, additional);
+        expect(result).toBe(expected);
+    }
+);
