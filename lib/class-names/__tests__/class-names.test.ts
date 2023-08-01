6174710624c9918f5e4706198193cc184ca4d941
meVarensev <vitalik.unit@gmail.com>
2023-08-01 18:16:48 +0500

    fix: class name test

--- lib/class-names/__tests__/class-names.test.ts
+++ lib/class-names/__tests__/class-names.test.ts
@@ -1,53 +1,12 @@
 import { classNames } from '../class-names';
 
-test('classNames function should return the base class when no mods and additional classes are provided', () => {
-    const cls = 'base-class';
-    const mods = {};
-    const additional = [];
-
+test.concurrent.each([
+    ['base-class', {}, [], 'base-class'],
+    ['base-class', { active: true, large: true, disabled: false }, ['extra-class', 'more-classes'], 'base-class extra-class more-classes active large'],
+    ['base-class', { active: true, large: false, disabled: true }, [], 'base-class active disabled'],
+    ['base-class', { active: true, large: undefined, disabled: true }, [], 'base-class active disabled'],
+])('classNames function should behave correctly with different inputs', async (cls, mods, additional, expected) => {
     const result = classNames(cls, mods, additional);
 
-    expect(result).toBe('base-class');
-});
-
-test('classNames function should combine base class, additional classes, and truthy mods', () => {
-    const cls = 'base-class';
-    const mods = {
-        active: true,
-        large: true,
-        disabled: false,
-    };
-    const additional = ['extra-class', 'more-classes'];
-
-    const result = classNames(cls, mods, additional);
-
-    expect(result).toBe('base-class extra-class more-classes active large');
-});
-
-test('classNames function should ignore falsy mods', () => {
-    const cls = 'base-class';
-    const mods = {
-        active: true,
-        large: false,
-        disabled: true,
-    };
-    const additional = [];
-
-    const result = classNames(cls, mods, additional);
-
-    expect(result).toBe('base-class active disabled');
-});
-
-test('classNames function should ignore undefined mods', () => {
-    const cls = 'base-class';
-    const mods = {
-        active: true,
-        large: undefined,
-        disabled: true,
-    };
-    const additional = [];
-
-    const result = classNames(cls, mods, additional);
-
-    expect(result).toBe('base-class active disabled');
+    expect(result).toBe(expected);
 });
